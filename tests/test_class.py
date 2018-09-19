b39f088c1044f9039e6fde8ce3a57258eb49d276
Kelly Rowland <kellylynnerowland@gmail.com>
2018-09-19 15:32:03 -0700

    filling out more setup information

--- tests/test_class.py
+++ tests/test_class.py
@@ -8,10 +8,13 @@ class TestSSHSpawner:
     def setup_class(cls):
         cls.SSHSpawner = SSHSpawner()
         cls.SSHSpawner.hub = Hub()
-        cls.SSHSpawner.hub.public_host = "host12345"
+        cls.SSHSpawner.hub.public_host = "testhost"
         orm_user = orm.User(name="testuser")
         cls.SSHSpawner.user = orm_user
-        cls.SSHSpawner.user.url = "http://www.example.com/"
+        cls.SSHSpawner.user.url = "http://localhost"
+        orm_server = orm.Server()
+        cls.SSHSpawner.user.server = orm_server
+        cls.SSHSpawner.user.server.cookie_name = "testcookie"
 
     def test_remote(self):
         port = self.SSHSpawner.remote_port
@@ -20,4 +23,7 @@ class TestSSHSpawner:
         assert(host == "remote_host")
 
     def test_env(self):
-        env = self.SSHSpawner.get_env()
+        env = self.SSHSpawner.user_env()
+        assert(env['JPY_USER'] == "testuser")
+        assert(env['JPY_COOKIE_NAME'] == "testcookie")
+        assert(env['PATH'] == "/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin")
