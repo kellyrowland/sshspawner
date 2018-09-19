5dd2d4f84d5d9fcfae187c8724468f030582ea29
Kelly Rowland <kellylynnerowland@gmail.com>
2018-09-19 14:33:10 -0700

    testing more setup

--- tests/test_class.py
+++ tests/test_class.py
@@ -1,10 +1,17 @@
 import pytest
+from jupyterhub.objects import Hub
+from jupyterhub import orm
 from sshspawner.sshspawner import SSHSpawner
 
 class TestSSHSpawner:
     @classmethod
     def setup_class(cls):
         cls.SSHSpawner = SSHSpawner()
+        cls.SSHSpawner.hub = Hub()
+        cls.SSHSpawner.hub.public_host = "host12345"
+        orm_user = orm.User(name="testuser")
+        cls.SSHSpawner.user = orm_user
+        cls.SSHSpawner.user.url = "http://www.example.com/"
 
     def test_remote(self):
         port = self.SSHSpawner.remote_port
@@ -12,3 +19,5 @@ class TestSSHSpawner:
         assert(port == "22")
         assert(host == "remote_host")
 
+    def test_env(self):
+        env = self.SSHSpawner.get_env()
