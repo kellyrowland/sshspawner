cb3b06181460595f4160ab6406e576164bea6f8e
Kelly Rowland <kellylynnerowland@gmail.com>
2018-10-02 16:58:55 -0700

    get remote port default

--- tests/test_class.py
+++ tests/test_class.py
@@ -19,24 +19,24 @@ def server():
 class TestSSHSpawner:
     @fixture()
     def spawner(self,server):
-        s = server
         self.SSHSpawner = SSHSpawner()
         SSHSpawner.hub = Hub()
-        for uid in s.users:
+        for uid in server.users:
             SSHSpawner.user.name = uid
 
-    def test_server(self,spawner):
+    def test_server(self,server,spawner):
         assert(SSHSpawner.user.name == "testuser")
-        # for uid in server.users:
-        #     with server.client(uid) as c:
-        #         port = SSHSpawner.remote_port
-        #         # loop = asyncio.get_event_loop()
-        #         # random_port = loop.run_until_complete(self.SSHSpawner.remote_random_port())
-        #         # assert(port == 80)
-        # SSHSpawner.hub.public_host = server.host
-        # assert(SSHSpawner.hub.public_host == "127.0.0.1")
+        for uid in server.users:
+            with server.client(uid) as c:
+                port = SSHSpawner.remote_port.default_value
+        #         loop = asyncio.get_event_loop()
+        #         random_port = loop.run_until_complete(SSHSpawner.remote_random_port())
+        #         assert(port == 80)
+        assert(port == "22")
+        SSHSpawner.hub.public_host = server.host
+        assert(SSHSpawner.hub.public_host == "127.0.0.1")
 
-    def test_remote(self,server):
+    def test_remote(self,server,spawner):
         assert(True)
         # SSHSpawner.remote_hosts = server.host.split()
         # loop = asyncio.get_event_loop()
@@ -52,9 +52,9 @@ class TestSSHSpawner:
         # assert(random_port == 80)
         # assert(SSHSpawner.user.name == "testuser")
 
-    def test_env(self):
+    def test_env(self,spawner):
         assert(True)
-        # env = self.SSHSpawner.user_env()
+        # env = SSHSpawner.user_env()
         # assert(env['JPY_USER'] == "testuser")
         # assert(env['JPY_COOKIE_NAME'] == "testcookie")
         # assert(env['PATH'] == "/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin")
