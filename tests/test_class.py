128c9b2289369f873d68b81bd5cbac3adc818adf
Kelly Rowland <kellylynnerowland@gmail.com>
2018-10-05 16:08:23 -0700

    expecting tests to fail

--- tests/test_class.py
+++ tests/test_class.py
@@ -8,7 +8,7 @@ from jupyterhub.user import User
 from pytest import fixture, yield_fixture
 from sshspawner.sshspawner import SSHSpawner
 
-@yield_fixture(scope= "session")
+@yield_fixture(scope = "session")
 def server():
     users = {
                 "testuser": "tests/sample-user-key"
@@ -16,35 +16,49 @@ def server():
     with mockssh.Server(users) as s:
         yield s
 
-@fixture(scope= "session")
+@fixture(scope = "session")
 def spawner(server):
+    uid = list(server.users)[0]
     ssh_spawner = SSHSpawner()
     ssh_spawner.hub = Hub()
     ssh_spawner.hub.public_host = server.host
+    ssh_spawner.hub.server.base_url = "/base/lab/"
     ssh_spawner.remote_hosts = server.host.split()
-    ssh_spawner.user = orm.User(name = list(server.users)[0])
+    ssh_spawner.ssh_keyfile = server._users[uid][0]
+    ssh_spawner.user = orm.User(name = uid)
     ssh_spawner.user.server = Server()
+    ssh_spawner.user.server.base_url = "/home/"
+    ssh_spawner.user.server.cookie_name = "testcookie"
+    ssh_spawner.user.url = "http://www.example.com/"
     return ssh_spawner
 
 class TestSSHSpawner:
-    def test_start(self,server,spawner):
+    def test_start(self,spawner):
         loop = asyncio.get_event_loop()
-        loop.run_until_complete(spawner.start())
+        start = loop.run_until_complete(spawner.start())
         port = spawner.remote_port
         host = spawner.remote_host
         assert(port == "22")
         assert(host == "127.0.0.1")
-        assert(spawner.user.name == "testuser")
+        assert(start is False)
 
-    def test_env(self,server,spawner):
-        spawner.user.server.cookie_name = "testcookie"
-        spawner.user.url = "http://www.example.com/"
+    def test_env(self,spawner):
         env = spawner.user_env()
-        assert(env['JPY_USER'] == "testuser")
-        assert(env['JPY_COOKIE_NAME'] == "testcookie")
+        assert(env['JUPYTERHUB_HOST'] == "127.0.0.1")
+        assert(env['JUPYTERHUB_USER'] == "testuser")
+        assert(env['JUPYTERHUB_PREFIX'] == "/base/lab/")
         assert(env['PATH'] == "/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin")
 
-    def test_random_port(self,server,spawner):
+    def test_exec(self, spawner):
         loop = asyncio.get_event_loop()
-        random_port = loop.run_until_complete(spawner.remote_random_port())
+        exc = loop.run_until_complete(spawner.exec_notebook(''))
+        assert(exc == -1)
+
+    def test_random_port(self,server,spawner):
+        spawner.remote_port_command = "python3 scripts/get_port.py"
+        with server.client(spawner.user.name) as c:
+            _, stdout, _ = c.exec_command(spawner.remote_port_command)
+            loop = asyncio.get_event_loop()
+            random_port = loop.run_until_complete(spawner.remote_random_port())
+        # assert isinstance(random_port, int)
         assert(True)
